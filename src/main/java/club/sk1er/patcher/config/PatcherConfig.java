af5d0a5ae3520c693a94fe7ecfd3e2a565ed3318
Wyvest <wyvestbusiness@gmail.com>
2023-02-14 14:13:57 -0500

    fix conflict

--- src/main/java/club/sk1er/patcher/config/PatcherConfig.java
+++ src/main/java/club/sk1er/patcher/config/PatcherConfig.java
@@ -639,8 +639,8 @@ public class PatcherConfig extends Config {
         Patcher.instance.getPatcherSoundConfig().openGui();
     }
 
-    @Property(
-        type = PropertyType.SWITCH, name = "Natural Capes",
+    @Switch(
+        name = "Natural Capes",
         description = "Changes some physics in capes to fix rotation bugs and look more natural.",
         category = "Miscellaneous", subcategory = "Rendering"
     )
