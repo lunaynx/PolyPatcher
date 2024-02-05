08d5e23abe9870acf3d230a7194dba36cede1764
ImToggle <98242902+ImToggle@users.noreply.github.com>
2024-02-05 15:28:59 +0800

    zoom

--- src/main/java/club/sk1er/patcher/hooks/EntityRendererHook.java
+++ src/main/java/club/sk1er/patcher/hooks/EntityRendererHook.java
@@ -17,10 +17,26 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @SuppressWarnings("unused")
 public class EntityRendererHook {
     private static final Minecraft mc = Minecraft.getMinecraft();
+    private static boolean zoomToggled = false;
     private static boolean isBeingHeld = false;
     private static float oldSensitivity;
     private static float partialTicks;
     public static float lastZoomModifier;
+
+    public static void fixMissingChunks() {
+        mc.renderGlobal.setDisplayListEntitiesDirty();
+    }
+
+    public static boolean getZoomState(boolean zoomKeyDown) {
+        if (zoomKeyDown) {
+            if (isBeingHeld) return zoomToggled;
+            isBeingHeld = true;
+            zoomToggled = !zoomToggled;
+        } else {
+            isBeingHeld = false;
+        }
+        return zoomToggled;
+    }
 
     public static boolean hasMap() {
         if (!PatcherConfig.mapBobbing || mc.thePlayer == null) return false;
@@ -32,6 +48,39 @@ public class EntityRendererHook {
         //$$ ItemStack offHandItem = mc.player.getHeldItemOffhand();
         //$$ return (mainHandItem != null && mainHandItem.getItem() instanceof ItemMap) || (offHandItem != null && offHandItem.getItem() instanceof ItemMap);
         //#endif
+    }
+
+    public static void reduceSensitivityWhenZoomStarts() {
+        oldSensitivity = mc.gameSettings.mouseSensitivity;
+        mc.gameSettings.mouseSensitivity = oldSensitivity * PatcherConfig.customZoomSensitivity;
+    }
+
+    public static void reduceSensitivityDynamically(float modifier) {
+        if (!PatcherConfig.dynamicZoomSensitivity || !ZoomHook.zoomed) return;
+        float sensitivity = oldSensitivity * PatcherConfig.customZoomSensitivity;
+        sensitivity *= modifier / lastZoomModifier;
+        mc.gameSettings.mouseSensitivity = sensitivity;
+    }
+
+    public static void resetSensitivity() {
+        mc.gameSettings.mouseSensitivity = oldSensitivity;
+    }
+
+    public static float getHandFOVModifier(float original) {
+        if (PatcherConfig.renderHandWhenZoomed && (ZoomHook.zoomed || (PatcherConfig.smoothZoomAnimation && ZoomHook.smoothZoomProgress > 0))) {
+            float f = 70f;
+            //#if MC==10809
+            Block block = ActiveRenderInfo.getBlockAtEntityViewpoint(mc.theWorld, mc.thePlayer, partialTicks);
+            //#else
+            //$$ IBlockState block = ActiveRenderInfo.getBlockStateAtEntityViewpoint(mc.world, mc.player, partialTicks);
+            //#endif
+
+            if (block.getMaterial() == Material.water) {
+                f = f * 60.0F / 70.0F;
+            }
+            return f;
+        }
+        return original;
     }
 
     @SubscribeEvent
