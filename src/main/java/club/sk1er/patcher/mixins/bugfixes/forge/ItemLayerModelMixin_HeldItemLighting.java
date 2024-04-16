5bdf1d9c610b8f0cc162913517f80389b64861f6
Mixces <aar.hcms@gmail.com>
2024-04-16 13:39:33 -0500

    Move bug fixes to Patcher from Overflow Animations

---
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/forge/ItemLayerModelMixin_HeldItemLighting.java
@@ -0,0 +1,32 @@
+package club.sk1er.patcher.mixins.bugfixes.forge;
+
+import com.google.common.base.Optional;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.vertex.VertexFormat;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.client.model.ItemLayerModel;
+import net.minecraftforge.client.model.TRSRTransformation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = ItemLayerModel.class, remap = false)
+public abstract class ItemLayerModelMixin_HeldItemLighting {
+
+    @Unique private static EnumFacing patcher$side;
+
+    @Inject(method = "buildSideQuad", at = @At(value = "HEAD"))
+    private static void patcher$captureSide(VertexFormat format, Optional<TRSRTransformation> transform, EnumFacing side, int tint, TextureAtlasSprite sprite, int u, int v, CallbackInfoReturnable<BakedQuad> cir) {
+        patcher$side = side;
+    }
+
+    @ModifyArg(method = "buildSideQuad", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/model/ItemLayerModel;buildQuad(Lnet/minecraft/client/renderer/vertex/VertexFormat;Lcom/google/common/base/Optional;Lnet/minecraft/util/EnumFacing;IFFFFFFFFFFFFFFFFFFFF)Lnet/minecraft/client/renderer/block/model/BakedQuad;"), index = 2, remap = false)
+    private static EnumFacing patcher$correctLighting(EnumFacing original) {
+        return patcher$side.getAxis() == EnumFacing.Axis.Y ? patcher$side.getOpposite() : patcher$side;
+    }
+
+}
---
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/render/item/RenderItemMixin_CullFace.java
@@ -0,0 +1,27 @@
+package club.sk1er.patcher.mixins.bugfixes.render.item;
+
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.block.model.ItemTransformVec3f;
+import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.client.resources.model.IBakedModel;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(RenderItem.class)
+public abstract class RenderItemMixin_CullFace {
+
+    @Shadow protected abstract boolean isThereOneNegativeScale(ItemTransformVec3f itemTranformVec);
+
+    @Inject(method = "renderItemModelTransform", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/RenderItem;renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/resources/model/IBakedModel;)V"))
+    private void patcher$cullFace(ItemStack stack, IBakedModel model, ItemCameraTransforms.TransformType cameraTransformType, CallbackInfo ci) {
+        if (isThereOneNegativeScale(model.getItemCameraTransforms().getTransform(cameraTransformType))) {
+            GlStateManager.cullFace(1028);
+        }
+    }
+
+}
