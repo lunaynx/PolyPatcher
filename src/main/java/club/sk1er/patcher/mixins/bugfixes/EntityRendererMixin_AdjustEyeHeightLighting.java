ee2e4055e64f8624d596175167eed23cb9b2f0e1
Mixces <aar.hcms@gmail.com>
2024-04-22 20:15:06 -0500

    Fix issue with "Additional fix for MC-51150"

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/EntityRendererMixin_AdjustEyeHeightLighting.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/EntityRendererMixin_AdjustEyeHeightLighting.java
@@ -1,19 +1,23 @@
 package club.sk1er.patcher.mixins.bugfixes;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
-import net.minecraft.entity.Entity;
 import net.minecraft.util.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixin_AdjustEyeHeightLighting {
 
     //#if MC==10809
-    @Redirect(method = "updateRenderer", at = @At(value = "NEW", target = "(Lnet/minecraft/entity/Entity;)Lnet/minecraft/util/BlockPos;"))
-    private BlockPos patcher$accountForEyes(Entity source) {
-        return new BlockPos(source.getPositionEyes(1.0F));
+    @Shadow
+    private Minecraft mc;
+
+    @ModifyArg(method = "updateRenderer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/WorldClient;getLightBrightness(Lnet/minecraft/util/BlockPos;)F"))
+    private BlockPos patcher$accountForEyes(BlockPos par1) {
+        return new BlockPos(mc.getRenderViewEntity().getPositionEyes(1.0F));
     }
     //#endif
 
