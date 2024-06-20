f0a17802c8f71d9abe0834b876f5316a3a479935
ev chang <wyvestbusiness@gmail.com>
2024-06-20 19:12:58 +0700

    bump version to 1.8.10

--- src/main/java/club/sk1er/patcher/config/OldPatcherConfig.java
+++ src/main/java/club/sk1er/patcher/config/OldPatcherConfig.java
@@ -2,31 +2,31 @@ package club.sk1er.patcher.config;
 
 // Developer use this in case they want to access values from the old Patcher that were removed / replaced
 public class OldPatcherConfig {
-    public static boolean nauseaEffect;
-    public static float fireOverlayOpacity = 1F;
-    public static boolean disableTitles;
-    public static float titleScale = 1.0F;
-    public static float titleOpacity = 1.0F;
-    public static boolean toggleTab;
-    public static boolean crosshairPerspective;
-    public static boolean showOwnNametag;
-    public static float riddenHorseOpacity = 1F;
-    public static boolean numberPing;
-    public static boolean cleanView;
-    public static boolean disableBlockBreakParticles;
-    public static boolean removeInvertFromCrosshair;
-    public static boolean shadowedNametagText;
-    public static boolean shadowedActionbarText;
-    public static boolean actionbarBackground;
-    public static boolean removeVerticalViewBobbing;
-    public static boolean staticParticleColor = false;
-    public static int maxParticleLimit = 4000;
-    public static boolean disableNametagBoxes;
-    public static boolean removeContainerBackground = false;
-    public static boolean guiCrosshair;
-    public static float tabOpacity = 1.0F;
-    public static int tabPlayerCount = 80;
-    public static boolean tabHeightAllow = true;
-    public static int tabHeight = 10;
-    public static boolean fixActionbarOverlap;
+    public static boolean nauseaEffect; // Patcher
+    public static float fireOverlayOpacity = 1F; // Patcher
+    public static boolean disableTitles; // VanillaHUD
+    public static float titleScale = 1.0F; // VanillaHUD
+    public static float titleOpacity = 1.0F; // VanillaHUD
+    public static boolean toggleTab; // VanillaHUD
+    public static boolean crosshairPerspective; // PolyCrosshair
+    public static boolean showOwnNametag; // PolyNametag
+    public static float riddenHorseOpacity = 1F; // Patcher
+    public static boolean numberPing; // VanillaHUD
+    public static boolean cleanView; // OverflowParticles
+    public static boolean disableBlockBreakParticles; // OverflowParticles
+    public static boolean removeInvertFromCrosshair; // PolyCrosshair
+    public static boolean shadowedNametagText; // PolyNametag
+    public static boolean shadowedActionbarText; // VanillaHUD
+    public static boolean actionbarBackground; // VanillaHUD
+    public static boolean removeVerticalViewBobbing; // OverflowAnimations
+    public static boolean staticParticleColor = false; // OverflowParticles
+    public static int maxParticleLimit = 4000; // OverflowParticles
+    public static boolean disableNametagBoxes; // PolyNametag
+    public static boolean removeContainerBackground = false; // Patcher
+    public static boolean guiCrosshair; // PolyCrosshair
+    public static float tabOpacity = 1.0F; // VanillaHUD
+    public static int tabPlayerCount = 80; // VanillaHUD
+    public static boolean tabHeightAllow = true; // VanillaHUD
+    public static int tabHeight = 10; // VanillaHUD
+    public static boolean fixActionbarOverlap; // VanillaHUD
 }
