56d8f6f265f7301867bb6e9bd3ae3677311b6f8c
Mixces <aar.hcms@gmail.com>
2024-07-12 01:21:15 -0500

    Bring back cached tooltips!

--- src/main/java/club/sk1er/patcher/Patcher.java
+++ src/main/java/club/sk1er/patcher/Patcher.java
@@ -27,6 +27,7 @@ import club.sk1er.patcher.util.enhancement.EnhancementManager;
 import club.sk1er.patcher.util.enhancement.ReloadListener;
 import club.sk1er.patcher.util.forge.EntrypointCaching;
 import club.sk1er.patcher.util.fov.FovHandler;
+import club.sk1er.patcher.util.item.TooltipHandler;
 import club.sk1er.patcher.util.keybind.FunctionKeyChanger;
 import club.sk1er.patcher.util.keybind.KeybindDropModifier;
 import club.sk1er.patcher.util.keybind.MousePerspectiveKeybindHandler;
@@ -135,7 +136,7 @@ public class Patcher {
             new TitleFix(), new LinuxKeybindFix(),
             new MetricsRenderer(), new HUDCaching(), new EntityRendererHook(),
             MinecraftHook.INSTANCE, ScreenshotPreview.INSTANCE,
-            new MousePerspectiveKeybindHandler()
+            new MousePerspectiveKeybindHandler(), TooltipHandler.INSTANCE
         );
 
         checkLogs();
