742df196ab606d92bf26ac8d141af9f53ab7b928
ev chang <wyvestbusiness@gmail.com>
2024-07-19 15:00:15 +0900

    implement our own pair class

--- src/main/java/club/sk1er/patcher/util/EventBusBenchmark.java
+++ src/main/java/club/sk1er/patcher/util/EventBusBenchmark.java
@@ -2,8 +2,6 @@ package club.sk1er.patcher.util;
 
 import club.sk1er.patcher.Patcher;
 import club.sk1er.patcher.util.chat.ChatUtilities;
-import it.unimi.dsi.fastutil.Pair;
-import it.unimi.dsi.fastutil.longs.LongLongMutablePair;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import me.kbrewster.eventbus.forge.KEventBus;
 import me.kbrewster.eventbus.forge.invokers.LMFInvoker;
@@ -23,17 +21,27 @@ public class EventBusBenchmark {
         }
     }
 
+    private static class LongPair {
+        public long first;
+        public long second;
+
+        public LongPair(long first, long second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
     public static void benchmark() {
         int runs = 5;
         List<String> messages = new ArrayList<>(runs * 6);
-        List<Pair<Long, Long>> registerTimes = new ArrayList<>(runs);
-        List<Pair<Long, Long>> postTimes = new ArrayList<>(runs);
-        List<Pair<Long, Long>> unregisterTimes = new ArrayList<>(runs);
+        List<LongPair> registerTimes = new ArrayList<>(runs);
+        List<LongPair> postTimes = new ArrayList<>(runs);
+        List<LongPair> unregisterTimes = new ArrayList<>(runs);
 
         for (int i = 0; i < runs; i++) {
-            Pair<Long, Long> registerTime = LongLongMutablePair.of(0L, 0L);
-            Pair<Long, Long> postTime = LongLongMutablePair.of(0L, 0L);
-            Pair<Long, Long> unregisterTime = LongLongMutablePair.of(0L, 0L);
+            LongPair registerTime = new LongPair(0L, 0L);
+            LongPair postTime = new LongPair(0L, 0L);
+            LongPair unregisterTime = new LongPair(0L, 0L);
             Patcher.instance.getLogger().info("Running test " + (i + 1) + " of " + runs);
             runTest(messages, registerTime, postTime, unregisterTime);
             Patcher.instance.getLogger().info("Done with test " + (i + 1) + " of " + runs);
@@ -49,19 +57,19 @@ public class EventBusBenchmark {
         long totalUnregisterKEventBus = 0;
         long totalUnregisterForge = 0;
 
-        for (Pair<Long, Long> time : registerTimes) {
-            totalRegisterKEventBus += time.first();
-            totalRegisterForge += time.second();
+        for (LongPair time : registerTimes) {
+            totalRegisterKEventBus += time.first;
+            totalRegisterForge += time.second;
         }
 
-        for (Pair<Long, Long> time : postTimes) {
-            totalPostKEventBus += time.first();
-            totalPostForge += time.second();
+        for (LongPair time : postTimes) {
+            totalPostKEventBus += time.first;
+            totalPostForge += time.second;
         }
 
-        for (Pair<Long, Long> time : unregisterTimes) {
-            totalUnregisterKEventBus += time.first();
-            totalUnregisterForge += time.second();
+        for (LongPair time : unregisterTimes) {
+            totalUnregisterKEventBus += time.first;
+            totalUnregisterForge += time.second;
         }
 
         String message1 = "Average time to register 10,000 listeners: " + (totalRegisterKEventBus / (double) runs) + "ms for KEventBus, " + (totalRegisterForge / (double) runs) + "ms for Forge";
@@ -81,7 +89,7 @@ public class EventBusBenchmark {
         }
     }
 
-    private static void runTest(List<String> messages, Pair<Long, Long> registerTimes, Pair<Long, Long> postTimes, Pair<Long, Long> unregisterTimes) {
+    private static void runTest(List<String> messages, LongPair registerTimes, LongPair postTimes, LongPair unregisterTimes) {
         ObjectArrayList<EventBusTest> keventbusListeners = new ObjectArrayList<>(10_000);
         ObjectArrayList<EventBusTest> forgeListeners = new ObjectArrayList<>(10_000);
         final KEventBus patcher$kEventBus = new KEventBus(new LMFInvoker(), e -> System.err.println("An exception occurred in a method: " + e.getMessage()));
@@ -93,7 +101,7 @@ public class EventBusBenchmark {
             keventbusListeners.add(listener);
         }
         long total1 = (System.nanoTime() - start) / 1_000_000;
-        registerTimes.first(total1);
+        registerTimes.first = total1;
         String message1 = "Registering 10,000 listeners took " + total1 + "ms for KEventBus";
         Patcher.instance.getLogger().info(message1);
         messages.add(message1);
@@ -105,7 +113,7 @@ public class EventBusBenchmark {
             forgeListeners.add(listener);
         }
         long total2 = (System.nanoTime() - start) / 1_000_000;
-        registerTimes.second(total2);
+        registerTimes.second = total2;
         String message3 = "Registering 10,000 listeners took " + total2 + "ms for Forge";
         Patcher.instance.getLogger().info(message3);
         messages.add(message3);
@@ -115,7 +123,7 @@ public class EventBusBenchmark {
             patcher$kEventBus.post(new EventBusTestEvent());
         }
         long total3 = (System.nanoTime() - start) / 1_000_000;
-        postTimes.first(total3);
+        postTimes.first = total3;
         String message5 = "Posting 1,000 events took " + total3 + "ms for KEventBus";
         Patcher.instance.getLogger().info(message5);
         messages.add(message5);
@@ -125,7 +133,7 @@ public class EventBusBenchmark {
             MinecraftForge.EVENT_BUS.post(new EventBusTestEvent());
         }
         long total4 = (System.nanoTime() - start) / 1_000_000;
-        postTimes.second(total4);
+        postTimes.second = total4;
         String message6 = "Posting 1,000 events took " + total4 + "ms for Forge";
         Patcher.instance.getLogger().info(message6);
         messages.add(message6);
@@ -135,7 +143,7 @@ public class EventBusBenchmark {
             patcher$kEventBus.unregister(listener);
         }
         long total5 = (System.nanoTime() - start) / 1_000_000;
-        unregisterTimes.first(total5);
+        unregisterTimes.first = total5;
         String message2 = "Unregistering 10,000 listeners took " + total5 + "ms for KEventBus";
         Patcher.instance.getLogger().info(message2);
         messages.add(message2);
@@ -145,7 +153,7 @@ public class EventBusBenchmark {
             MinecraftForge.EVENT_BUS.unregister(listener);
         }
         long total6 = (System.nanoTime() - start) / 1_000_000;
-        unregisterTimes.second(total6);
+        unregisterTimes.second = total6;
         String message4 = "Unregistering 10,000 listeners took " + total6 + "ms for Forge";
         Patcher.instance.getLogger().info(message4);
         messages.add(message4);
