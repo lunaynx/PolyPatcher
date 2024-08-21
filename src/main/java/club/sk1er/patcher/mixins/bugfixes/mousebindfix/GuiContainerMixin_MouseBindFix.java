f814781aa935a051b5a3eb7abd20912f4fd4261f
ev chang <wyvestbusiness@gmail.com>
2024-08-22 01:38:24 +0700

    Revert "Allow drop key to work properly with side mouse buttons"
    This reverts commit 9e268dad64a8aca88bbe4e6eb47926690ecaae02.

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/mousebindfix/GuiContainerMixin_MouseBindFix.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/mousebindfix/GuiContainerMixin_MouseBindFix.java
@@ -2,34 +2,18 @@ package club.sk1er.patcher.mixins.bugfixes.mousebindfix;
 
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiContainer.class)
 public abstract class GuiContainerMixin_MouseBindFix extends GuiScreen {
-    @Shadow
-    private Slot theSlot;
-
-    @Shadow
-    protected abstract void handleMouseClick(Slot par1, int par2, int par3, int par4);
-
     @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
     private void patcher$checkCloseClick(int mouseX, int mouseY, int mouseButton, CallbackInfo ci) {
-        int keyCode = mouseButton - 100;
-        if (keyCode == mc.gameSettings.keyBindInventory.getKeyCode()) {
+        if (mouseButton - 100 == mc.gameSettings.keyBindInventory.getKeyCode()) {
             mc.thePlayer.closeScreen();
             ci.cancel();
-        }
-        if (theSlot != null && theSlot.getHasStack()) {
-            if (keyCode == mc.gameSettings.keyBindPickBlock.getKeyCode()) {
-                handleMouseClick(theSlot, theSlot.slotNumber, 0, 3);
-            } else if (keyCode == mc.gameSettings.keyBindDrop.getKeyCode()) {
-                handleMouseClick(theSlot, theSlot.slotNumber, isCtrlKeyDown() ? 1 : 0, 4);
-            }
         }
     }
 }
