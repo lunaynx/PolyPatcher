b7148041ff30fa659610ad6c1f33d1b4825dfee8
ev chang <wyvestbusiness@gmail.com>
2024-08-22 01:40:02 +0700

    Reapply "add 1.12.2 compatibility to "Allow drop key to work properly with side mouse buttons""
    This reverts commit 47e7dc9fca43f486bed8e79855ba26de00e94fcb.

--- src/main/java/club/sk1er/patcher/mixins/bugfixes/mousebindfix/GuiContainerMixin_MouseBindFix.java
+++ src/main/java/club/sk1er/patcher/mixins/bugfixes/mousebindfix/GuiContainerMixin_MouseBindFix.java
@@ -2,6 +2,9 @@ package club.sk1er.patcher.mixins.bugfixes.mousebindfix;
 
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
+//#if MC==11202
+//$$ import net.minecraft.inventory.ClickType;
+//#endif
 import net.minecraft.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,19 +14,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiContainer.class)
 public abstract class GuiContainerMixin_MouseBindFix extends GuiScreen {
+    //#if MC==10809
     @Shadow
     private Slot theSlot;
 
     @Shadow
     protected abstract void handleMouseClick(Slot par1, int par2, int par3, int par4);
+    //#else
+    //$$ @Shadow
+    //$$ private Slot hoveredSlot;
+    //$$
+    //$$ @Shadow
+    //$$ protected abstract void handleMouseClick(Slot slotIn, int slotId, int mouseButton, ClickType type);
+    //#endif
 
-    @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "mouseClicked", at = @At("HEAD"))
     private void patcher$checkCloseClick(int mouseX, int mouseY, int mouseButton, CallbackInfo ci) {
         int keyCode = mouseButton - 100;
         if (keyCode == mc.gameSettings.keyBindInventory.getKeyCode()) {
             mc.thePlayer.closeScreen();
-            ci.cancel();
         }
+        //#if MC==10809
         if (theSlot != null && theSlot.getHasStack()) {
             if (keyCode == mc.gameSettings.keyBindPickBlock.getKeyCode()) {
                 handleMouseClick(theSlot, theSlot.slotNumber, 0, 3);
@@ -31,5 +42,14 @@ public abstract class GuiContainerMixin_MouseBindFix extends GuiScreen {
                 handleMouseClick(theSlot, theSlot.slotNumber, isCtrlKeyDown() ? 1 : 0, 4);
             }
         }
+        //#else
+        //$$ if (hoveredSlot != null && hoveredSlot.getHasStack()) {
+        //$$    if (mc.gameSettings.keyBindPickBlock.isActiveAndMatches(keyCode)) {
+        //$$        handleMouseClick(hoveredSlot, hoveredSlot.slotNumber, 0, ClickType.CLONE);
+        //$$    } else if (mc.gameSettings.keyBindDrop.isActiveAndMatches(keyCode)) {
+        //$$        handleMouseClick(hoveredSlot, hoveredSlot.slotNumber, isCtrlKeyDown() ? 1 : 0, ClickType.THROW);
+        //$$    }
+        //$$ }
+        //#endif
     }
 }
