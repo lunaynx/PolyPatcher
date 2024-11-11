911aa28df15aaeb2e925560275ec1581cec30098
MicrocontrollersDev <66657148+MicrocontrollersDev@users.noreply.github.com>
2024-11-11 06:16:12 -0600

    clear sin table (#78)

--- src/main/java/club/sk1er/patcher/mixins/performance/MathHelperMixin_CompactLUT.java
+++ src/main/java/club/sk1er/patcher/mixins/performance/MathHelperMixin_CompactLUT.java
@@ -2,11 +2,24 @@ package club.sk1er.patcher.mixins.performance;
 
 import me.jellysquid.mods.lithium.common.util.math.CompactSineLUT;
 import net.minecraft.util.MathHelper;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MathHelper.class)
 public class MathHelperMixin_CompactLUT {
+    @Mutable
+    @Shadow
+    @Final
+    private static float[] SIN_TABLE;
+
+    @SuppressWarnings("InstantiationOfUtilityClass")
+    @Inject(method = "<clinit>", at = @At("RETURN"))
+    private static void clearSinTable(CallbackInfo ci) {
+        new CompactSineLUT(); // Force class initialisation
+        MathHelperMixin_CompactLUT.SIN_TABLE = null;
+    }
 
     /**
      * @author Wyvest
